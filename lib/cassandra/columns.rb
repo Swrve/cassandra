5165a9635d00bd3306409046ee7ace8a3ad4aa73
Ian O'Connell <ian.oconnell@swrve.com>
2011-04-20 20:15:15 -0700

    Update for changed thrift naming scheme

--- lib/cassandra/columns.rb
+++ lib/cassandra/columns.rb
@@ -69,7 +69,7 @@ class Cassandra
     def columns_to_hash_for_classes(columns, column_name_class, sub_column_name_class = nil)
       hash = OrderedHash.new
       Array(columns).each do |c|
-        c = c.super_column || c.column if c.is_a?(CassandraThrift::ColumnOrSuperColumn) or c.is_a?(CassandraThrift::Counter)
+        c = c.super_column || c.column if c.is_a?(CassandraThrift::ColumnOrSuperColumn) or c.is_a?(CassandraThrift::CounterColumn)
         case c
         when CassandraThrift::SuperColumn, CassandraThrift::CounterSuperColumn
           hash.[]=(column_name_class.new(c.name), columns_to_hash_for_classes(c.columns, sub_column_name_class)) # Pop the class stack, and recurse
